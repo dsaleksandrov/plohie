70192dca194a10b093a7ed8ee122ab0b8691170e
dsaleksandrov <dprojectss@gmail.com>
2012-03-04 01:06:34 +0400

    dsfsdfsdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plohie::Application.config.secret_token = '6b4a195ee1418c77dcb69082d5f406e144ba534480f016e2d990a31293dd566c4e79979db0fbf91c1463d9d49537d8ba05893cd8a1ee1dec50fa5f3f07bbb17d'
